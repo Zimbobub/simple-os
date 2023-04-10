6edcea4d11177e4a379bd24f7562274455523017
Zimbobub <89832179+Zimbobub@users.noreply.github.com>
2023-04-10 18:02:10 +1000

    cd command now works with names

--- src/apps/cd.asm
+++ src/apps/cd.asm
@@ -1,23 +1,27 @@
 ; change directory
 
-; temporarily uses sector numbers as arg
-; also only supports 1 digit sector nums as i havent added BCD yet
+; TODO: add special cases for root directory
 
 push ax
+push bx
 push si
 
 ; shift the command away
 add si, 3
 
-; get the int value of arg
-mov ax, [si]
-sub ax, '0'
+; get the sector number
+; BX: sector to search
+; SI: filename to search for
+mov bx, [WORKING_DIRECTORY_INFO]
+call findSectorByName
 
 ; set working dir
+; AX: sector number (already set by above function)
 call setWorkingDirectory
 
 ; return
 pop si
+pop bx
 pop ax
 
 jmp main
