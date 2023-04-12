433e6edee792718ece556e03df26fb9dce7e5612
Zimbobub <89832179+Zimbobub@users.noreply.github.com>
2023-04-12 17:35:58 +1000

    added some error handling for mkdir cmd

--- src/apps/mkdir.asm
+++ src/apps/mkdir.asm
@@ -5,9 +5,22 @@ push ax
 push si
 
 add si, 6
+
+; check if there was no argument ([SI] will be null)
+mov byte al, [si]
+or al, al
+jz .noArgError
+
 mov ax, [WORKING_DIRECTORY_INFO]    ; offset of 0 is the working dir's sector #
 call createDirectory
 
-pop si
-pop ax
-jmp main
+.exit:
+    pop si
+    pop ax
+    jmp main
+
+.noArgError:
+    mov si, .noArgErrorMsg
+    call puts
+    jmp .exit
+.noArgErrorMsg: db 'Please specify a name for the directory', ENDL, 0
